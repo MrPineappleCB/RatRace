30a36c4f134e7d84cb062cbc1bc46f5e66403b70
Lyanair <Rloughran280@gmail.com>
2025-10-14 13:52:00 +0100

    Scripts

---
+++ Rat Race Prototype 2/Assets/Scripts/Enemy.cs
@@ -0,0 +1,160 @@
+using UnityEngine;
+using Pathfinding;
+using System.Data.Common;
+using Unity.VisualScripting;
+using NUnit.Framework;
+
+public class Enemy : MonoBehaviour
+{
+    [SerializeField] private Transform groundCheck;
+    [SerializeField] private LayerMask groundLayer;
+    [SerializeField] private Transform wallCheck;
+    [SerializeField] private LayerMask wallLayer;
+
+    [SerializeField] private float jumpStrength = 10f;
+    [SerializeField] private float wallSlideSpeed = 2f;
+
+    private bool isWallSliding;
+    private bool isWallJumping;
+    private float wallJumpingDirection;
+    private float wallJumpingTime = 0.2f;
+    private float wallJumpingCounter;
+    private float wallJumpingDuration = 1f;
+    private Vector2 wallJumpingPower = new Vector2(5f, 7f);
+
+    public Transform target;
+
+    public float speed = 200f;
+    public float nextWaypointDistance = 3f;
+
+    public Transform enemyGFX;
+
+    Path path;
+    int currentWaypoint = 0;
+    bool EndofPath = false;
+
+    Seeker seeker;
+    Rigidbody2D rb;
+
+
+    void Start()
+    {
+        seeker = GetComponent<Seeker>();
+        rb = GetComponent<Rigidbody2D>();
+
+        InvokeRepeating("UpdatePath", 0f, .5f);
+        
+    }
+    
+    void UpdatePath()
+    {
+        if (seeker.IsDone())
+        seeker.StartPath(rb.position, target.position, OnPathComplete);
+    }
+    void OnPathComplete(Path p)
+    {
+        if (!p.error)
+        {
+            path = p;
+            currentWaypoint = 0;
+        }
+    }
+
+    public bool isGrounded()
+    {
+        return Physics2D.OverlapCircle(groundCheck.position, 0.2f, groundLayer); //Returns true if circle around the position of ground check is touching/inside the groundlayer
+    }
+    public bool isWallCling()
+    {
+        return Physics2D.OverlapCircle(wallCheck.position, 0.2f, wallLayer); //Returns true if circle around the position of ground check is touching/inside the walllayer
+    }
+    void FixedUpdate()
+    {
+        if (path == null)
+            return;
+
+        if (currentWaypoint >= path.vectorPath.Count)
+        {
+            EndofPath = true;
+            return;
+        }
+        else
+        {
+            EndofPath = false;
+        }
+
+        Vector2 direction = ((Vector2)path.vectorPath[currentWaypoint] - rb.position).normalized;
+        Vector2 force = direction * speed * Time.deltaTime;
+        Vector2 velocity = rb.linearVelocity;
+
+        velocity.x = force.x;
+        rb.linearVelocity = velocity;
+
+        if (direction.y > transform.position.y && isGrounded())
+        {
+            rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpStrength);
+        }
+
+        float distance = Vector2.Distance(rb.position, path.vectorPath[currentWaypoint]);
+
+        if (distance < nextWaypointDistance)
+        {
+            currentWaypoint++;
+        }
+
+        if (force.x >= 0.01f)
+        {
+            enemyGFX.localScale = new Vector3(-1f, 1f, 1f);
+        }
+        else if (force.x <= -0.01f)
+        {
+            enemyGFX.localScale = new Vector3(1f, 1f, 1f);
+        }
+
+        WallSlide();
+        WallJump();
+    }
+    private void WallSlide()
+    {
+        if (isWallCling() && !isGrounded() && rb.linearVelocity.x != 0f)
+        {
+            isWallSliding = true;
+            rb.linearVelocity = new Vector2(rb.linearVelocity.x, Mathf.Clamp(rb.linearVelocity.y, -wallSlideSpeed, float.MaxValue));
+        }
+        else
+        {
+            isWallSliding = false;
+        }
+    }
+    private void WallJump()
+    {
+        Vector2 direction = ((Vector2)path.vectorPath[currentWaypoint] - rb.position).normalized;   
+        
+        if (isWallSliding)
+        {
+            isWallJumping = false;
+            wallJumpingDirection = -transform.localScale.x;
+            wallJumpingCounter = wallJumpingTime;
+
+            CancelInvoke(nameof(StopWallJumping));
+        }
+        else
+        {
+            wallJumpingCounter -= Time.deltaTime;
+        }
+
+        if (direction.y > transform.position.y && isWallCling() && wallJumpingCounter > 0f)
+        {
+            isWallJumping = true;
+            rb.linearVelocity = new Vector2(wallJumpingDirection * wallJumpingPower.x, wallJumpingPower.y);
+            wallJumpingCounter = 0f;
+
+            Invoke(nameof(StopWallJumping), wallJumpingDuration);
+        }
+    }
+    private void StopWallJumping()
+    {
+        isWallJumping = false;
+    }
+
+}

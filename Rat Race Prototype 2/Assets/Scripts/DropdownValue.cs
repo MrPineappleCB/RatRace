5bde03255ed8d1c7d3dd4ea0f9e58b0c46e5ca64
Lyanair <Rloughran280@gmail.com>
2025-11-27 17:24:21 +0000

    Main menu and laps 95% done
    Few little things here and there to still touch up

---
+++ Rat Race Prototype 2/Assets/Scripts/DropdownValue.cs
@@ -0,0 +1,26 @@
+using TMPro;
+using UnityEngine;
+
+public class DropdownValue : MonoBehaviour
+{
+    [SerializeField] private TMP_Dropdown dropdown;
+    [SerializeField] private MainMenu mainmenu;
+
+    public void GetDifficultyValue()
+    {
+        int pickedEntryIndex = dropdown.value;
+        mainmenu.dropdifficulty = pickedEntryIndex;
+    }
+
+    public void GetLapValue()
+    {
+        int pickedEntryIndex = dropdown.value;
+        mainmenu.droplap = pickedEntryIndex;
+    }
+
+    public void GetLengthValue()
+    {
+        int pickedEntryIndex = dropdown.value;
+        mainmenu.droplength = pickedEntryIndex;
+    }
+}
--- Rat Race Prototype 2/Assets/Scripts/LevelGen.cs
+++ Rat Race Prototype 2/Assets/Scripts/LevelGen.cs
@@ -1,18 +1,31 @@
 
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
+using UnityEngine.Rendering.Universal.Internal;
 
 public class LevelGen : MonoBehaviour
 {
     [SerializeField] public float levelLength = 0f;
     public static List<GameObject> myListObjects = new List<GameObject>();
+    public static List<GameObject> lapList = new List<GameObject>();
     public int genCounter = 0;
-    float savedwidth = 0;
+    public int lapCounter = 1;
+    public float savedwidth = 0;
     public float totaldist = 0;
     public float finaldist = 0f;
+    float savdist;
+    float lapdist;
+    float enddist = 7.5f;
+    public GameObject gamemanager;
+    public MainMenu mainMenu;
   
     void Start()
     {
+        gamemanager = GameObject.FindGameObjectWithTag("GameManager");
+        mainMenu = gamemanager.GetComponent<MainMenu>();
+        levelLength = mainMenu.laplength;
+
         Object[] subListObjects = Resources.LoadAll("LevelPrefabs", typeof(GameObject));
 
         foreach (GameObject subListObject in subListObjects)
@@ -25,14 +38,30 @@ public class LevelGen : MonoBehaviour
         {
             int randSegment = Random.Range(0, myListObjects.Count);
             GameObject myObj = Instantiate(myListObjects[randSegment]) as GameObject;
+            lapList.Add(myObj);
             float width = myObj.GetComponent<SpriteRenderer>().bounds.size.x;
             transform.position = new Vector3((width / 2 + savedwidth / 2) + totaldist + 15, -8, 0);
             myObj.transform.position = transform.position;
             totaldist = (width / 2) + (savedwidth / 2) + totaldist;
             savedwidth = width;
-            finaldist = myObj.transform.position.x + (width / 2) + (15 / 2);
+            finaldist = myObj.transform.position.x + (savedwidth / 2) + enddist;
+            savdist = finaldist;
             genCounter++;
         }
+
+        while (lapCounter < mainMenu.lap)
+        {
+            for (int i=0; i< lapList.Count; i++)
+            {
+                GameObject lapObj = Instantiate(lapList[i]) as GameObject;
+                float width = lapObj.GetComponent<SpriteRenderer>().bounds.size.x;
+                lapObj.transform.position = new Vector3(lapObj.transform.position.x + savdist - enddist, -8, 0);
+                lapdist = lapObj.transform.position.x + (width/2) + enddist;
+            }
+            savdist = lapdist;
+            lapCounter++;
+        }
+
 
         AstarPath.active.Scan();    
     }
